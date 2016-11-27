702fb528508afe7eb7ac3cec4a540e1db2b9d988
Michael Sevilla <mikesevilla3@gmail.com>
2016-11-26 22:39:54 -0800

    benchmark: add creates
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

---
+++ creates/benchmark-compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+
+./ansible-playbook.sh ../workloads/compile.yml
---
+++ creates/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+# setup the docker command
+RUN="docker run -it --rm --net host -v $HOME/.ssh:/root/.ssh -w /root"
+ANSIBLE="michaelsevilla/ansible --forks 50 --skip-tags package-install,with_pkg"
+CEPH_ANSIBLE="$RUN -v `pwd`/site/roles/ceph-ansible:/root $ANSIBLE"
+SRL_ANSIBLE="$RUN -v `pwd`/site:/root $ANSIBLE"
+
+# move configuration files
+cp site/* site/roles/ceph-ansible || true
+cp -r site/group_vars site/roles/ceph-ansible/
+
+# cleanup and start ceph
+$SRL_ANSIBLE cleanup.yml
+$CEPH_ANSIBLE ceph.yml cephfs.yml
+$SRL_ANSIBLE ceph_pgs.yml ceph_monitor.yml ceph_wait.yml
