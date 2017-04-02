2943589b7963a6efbf0a25c13ce39f93a8caf272
Michael Sevilla <mikesevilla3@gmail.com>
2017-04-01 18:00:34 -0700

    microbenchmarks: add isolation experiments
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/fuse/scale-clients-isolated/run.sh
+++ experiments/fuse/scale-clients-isolated/run.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 set -ex
+NFILES=98000
 
 # setup the docker command
 RUN="docker run -it --rm --net host -v $HOME/.ssh:/root/.ssh -w /root"
 ANSIBLE="michaelsevilla/ansible --forks 50 --skip-tags package-install,with_pkg"
 CEPH_ANSIBLE="$RUN -v `pwd`/site/roles/ceph-ansible:/root $ANSIBLE"
 SRL_ANSIBLE="$RUN -v `pwd`/site:/root $ANSIBLE"
-
-#for nclients in 2 3 4 5 6 7 8; do
-#  site="nojournal-cache"
-#  for run in `seq 0 2`; do
-#    for job in "creates" "creates-touchstream" "creates-stat" "creates-touch"; do
 
 for nclients in 2 3 4 5 6 7; do
   site="nojournal-cache"
@@ -35,17 +31,12 @@ for nclients in 2 3 4 5 6 7; do
         $CEPH_ANSIBLE ceph.yml #cephfs.yml
         $SRL_ANSIBLE ceph_pgs.yml ceph_monitor.yml ceph_wait.yml
   
-        ## warmup and get baseline
-        #for i in `seq 0 2`; do
-        #  ./ansible-playbook.sh -e site=$results -e nfiles=100000 ../workloads/warmup.yml
-        #done
-  
         # run job
-        ./ansible-playbook.sh -e site=$results -e nfiles=10 ../workloads/setup_mdtest.yml
+        ./ansible-playbook.sh -e site=$results -e nfiles=$NFILES ../workloads/setup_mdtest.yml
         if [ $decoupled -eq 1 ]; then
-          ./ansible-playbook.sh -e site=$results -e nfiles=10 ../workloads/decouple.yml
+          ./ansible-playbook.sh -e site=$results -e nfiles=$NFILES ../workloads/decouple.yml
         fi
-        ./ansible-playbook.sh -e site=$results -e nfiles=10 -e drop_delay=30 ../workloads/${job}.yml
+        ./ansible-playbook.sh -e site=$results -e nfiles=$NFILES -e drop_delay=30 ../workloads/${job}.yml
         ./ansible-playbook.sh -e site=$results collect.yml
       done
     done
--- experiments/run.sh
+++ experiments/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 #for w in "droplease-cache" "droplease-nocache"; do
-for w in "droplease-cache-creates" "droplease-nocache-creates"; do
+#for w in "droplease-cache-creates" "droplease-nocache-creates"; do
+for w in "fuse/scale-clients-isolated" "fuse/scale-merge"; do
   cd $w
   ./run.sh
   cd -
